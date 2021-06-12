3056e8d1194be23ea7a23a226b42fd60d2780c5a
annakhuseinova <annakhuseinova@yandex.ru>
2021-06-12 18:07:14 +0300

    Initial commit

---
+++ src/test/java/com/annakhuseinova/springcloudstreamsavro/SpringCloudStreamsAvroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.annakhuseinova.springcloudstreamsavro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamsAvroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
